aebea55665852a0e56f55a0c026ec19be70a41f8
rickysiauw <37130004+rickysiauw@users.noreply.github.com>
2018-04-17 18:05:48 +0700

    Add files via upload

---
+++ UAS(Package).py
@@ -0,0 +1,18 @@
+import ric.satu
+import ric.dua
+import ric.tiga
+import ric.empat
+import ric.lima
+import ric.enam
+
+print (ric.satu.fa())
+
+print (ric.dua.fb())
+
+print (ric.tiga.fc())
+
+print (ric.empat.fd())
+
+print (ric.lima.fe())
+
+print (ric.enam.ff())
